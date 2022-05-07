d733bd0b06ce428a0549cecf7291250ce4e9424d
ignakio <nacho.cladera@hotmail.com>
2022-05-07 11:56:54 +0000

    Added LOCAL STORAGE to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,3 +39,65 @@ if (document.readyState === "complete" || document.readyState === "interactive")
 } else {
     document.addEventListener("DOMContentLoaded", loadInitialData);
 }
+
+//LOCAL STORAGE
+// Function to store items data locally
+function saveItem(action, itenName){
+    // Compare two strings for packed items
+    if (action.localeCompare('packed') ===0){
+        var currentSavedItemsPacked = localStorage.getItem('packed');
+        if (currentSavedItemsPacked=== null){
+            const itemsPacked = [itenName];
+            /* Storing Objects in HTML5 localStorage original line of code with modification for project from: https://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage */
+            localStorage.setItem('packed',JSON.stringify(itemsPacked));
+        }
+        else{
+            /* Return data and convert text into a JavaScript object original line of code with modification for project from: https://stackoverflow.com/questions/35273539/json-parse-from-localstorage-issue */
+            var currentPackedItems = JSON.parse(localStorage.getItem('packed'));
+            // add item to list
+            currentPackedItems.push(itenName);
+            localStorage.setItem('packed',JSON.stringify(currentPackedItems));
+        }
+    }
+    // Compare two strings for to buy items
+    if (action.localeCompare('bought') ===0){
+        var currentSavedItemsBought = localStorage.getItem('bought');
+        if (currentSavedItemsBought=== null){
+            const itemsBought = [itenName];
+            localStorage.setItem('bought',JSON.stringify(itemsBought));
+        }
+        else{
+            var currentBoughtItems = JSON.parse(localStorage.getItem('bought'));
+            // add item to list
+            currentBoughtItems.push(itenName);
+            localStorage.setItem('bought',JSON.stringify(currentBoughtItems));
+        }
+    }
+}
+// To remove items from lists once checked in storage
+function removeItem(action, item){
+    // To retrieve it from the store and convert
+    var currentPackedItems = JSON.parse(localStorage.getItem('packed')) || [];
+    var currentBoughtItems = JSON.parse(localStorage.getItem('bought')) || [];
+    // To check if item on list
+    if (action.localeCompare('bought') ===0){
+        // To find item
+        const elementIndex = currentBoughtItems.indexOf(item);
+        console.log("trying to find element", item, elementIndex);
+        if (elementIndex > -1){
+            // To remove element
+            currentBoughtItems.splice(elementIndex, 1);
+            /* Put the object into storage orginal line of code with modifications for project from https://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage */
+            localStorage.setItem('bought',JSON.stringify(currentBoughtItems));
+        }
+    }
+    if (action.localeCompare('packed') ===0){
+        // To find item
+        const elementIndex = currentSavedItemsPacked.indexOf(item);
+        if (elementIndex > -1){
+            // To remove element
+            currentSavedItemsPacked.splice(elementIndex, 1);
+            localStorage.setItem('packed',JSON.stringify(currentSavedItemsPacked));
+        }
+    }
+}
